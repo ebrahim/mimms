8887c0348fe364303000086b9a64dc2eff4cea1f
Ebrahim Mohammadi <ebrahim@mohammadi.ir>
2012-05-28 21:30:35 +0430

    Fixed setup.py to reflect correct version number and homepage URL

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
   name='mimms',
-  version='3.2',
+  version='3.2.2',
   description='mimms is an mms (e.g. mms://) stream downloader',
   author='Wesley J. Landaker',
   author_email='wjl@icecavern.net.',
   license='GPLv3',
-  url='http://savannah.nongnu.org/projects/mimms/',
+  url='http://code.ebrahim.ir/mimms/',
   packages=['libmimms'],
   scripts=['mimms'],
   data_files=[
